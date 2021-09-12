6df231ce033f1991b557fb84e36b282883b917d5
ohfp <1813007-ohfp@users.noreply.gitlab.com>
2021-09-12 19:13:50 +0200

    add update information to AppImage

--- build_appimage.sh
+++ build_appimage.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-printf "\n\n------------------------------------ APPIMAGE BUILD -----------------------------------------\n";
+printf "\n\n------------------------------------ APPIMAGE BUILD -----------------------------------------\n"
 
 # Aborts the script upon any faliure
-set -e;
+set -e
 
 # Sets up script variables
 BINARY_TARBALL=$1
 APPIMAGE_FILE=$2
-_SCRIPT_FOLDER=$(realpath $(dirname $0));
-_BINARY_TARBALL_EXTRACTED_FOLDER=$_SCRIPT_FOLDER/librewolf;
-_BUILD_APPIMAGE_FILE=$_SCRIPT_FOLDER/LibreWolf.AppImage;
-_APPIMAGETOOL_DOWNLOAD_URL=https://github.com/AppImage/AppImageKit/releases/latest/download/appimagetool-${CARCH}.AppImage;
-_APPIMAGETOOL_EXTRACTED_FOLDER=$_SCRIPT_FOLDER/squashfs-root;
-_APPIMAGETOOL_FILE=$_SCRIPT_FOLDER/appimagetool;
+_SCRIPT_FOLDER=$(realpath $(dirname $0))
+_BINARY_TARBALL_EXTRACTED_FOLDER=$_SCRIPT_FOLDER/librewolf
+_BUILD_APPIMAGE_FILE=$_SCRIPT_FOLDER/LibreWolf.${CARCH}.AppImage
+_APPIMAGETOOL_DOWNLOAD_URL=https://github.com/AppImage/AppImageKit/releases/latest/download/appimagetool-${CARCH}.AppImage
+_APPIMAGETOOL_EXTRACTED_FOLDER=$_SCRIPT_FOLDER/squashfs-root
+_APPIMAGETOOL_FILE=$_SCRIPT_FOLDER/appimagetool
 _APPIMAGE_CONTENT_FOLDER=$_SCRIPT_FOLDER/content
 
 # Installs needed dependencies
@@ -24,11 +24,11 @@ else
   TARBALL_URL=${TARBALL_URL_X86_64}
 fi
 
-if [[ ! -z "${TARBALL_URL}" ]];then
+if [[ ! -z "${TARBALL_URL}" ]]; then
   wget "${TARBALL_URL}"
 fi
 
-if [[ ! -f "${BINARY_TARBALL}" ]];then
+if [[ ! -f "${BINARY_TARBALL}" ]]; then
   echo "Tarball not provided via pipeline or download."
   exit 1
 fi
@@ -38,19 +38,19 @@ if [[ $CARCH == 'aarch64' ]]; then
 fi
 
 # Extracts the binary tarball
-printf "\nExtracting librewolf binary tarball\n";
-mkdir $_BINARY_TARBALL_EXTRACTED_FOLDER;
-tar -xvf $BINARY_TARBALL -C $_BINARY_TARBALL_EXTRACTED_FOLDER;
+printf "\nExtracting librewolf binary tarball\n"
+mkdir "$_BINARY_TARBALL_EXTRACTED_FOLDER"
+tar -xvf "$BINARY_TARBALL" -C "$_BINARY_TARBALL_EXTRACTED_FOLDER"
 
 # Copy appimage resources to main tarball
-printf "Copying AppImage resources to binary tarball folder\n";
-cp -vrT $_APPIMAGE_CONTENT_FOLDER $_BINARY_TARBALL_EXTRACTED_FOLDER;
+printf "Copying AppImage resources to binary tarball folder\n"
+cp -vrT "$_APPIMAGE_CONTENT_FOLDER" "$_BINARY_TARBALL_EXTRACTED_FOLDER"
 
 # Downloads appimage tool
-printf "\nDownloading AppImage Tool\n";
-apt -qq update && apt -qqy install wget;
-wget $_APPIMAGETOOL_DOWNLOAD_URL -O $_APPIMAGETOOL_FILE;
-chmod +x $_APPIMAGETOOL_FILE;
+printf "\nDownloading AppImage Tool\n"
+apt -qq update && apt -qqy install wget
+wget "$_APPIMAGETOOL_DOWNLOAD_URL" -O "$_APPIMAGETOOL_FILE"
+chmod +x "$_APPIMAGETOOL_FILE"
 
 # add appstream metadata
 install -Dvm644 "$_BINARY_TARBALL_EXTRACTED_FOLDER/io.gitlab.librewolf-community.appdata.xml" "$_BINARY_TARBALL_EXTRACTED_FOLDER/usr/share/metainfo/io.gitlab.librewolf-community.appdata.xml"
@@ -63,17 +63,19 @@ install -D -m644 "$_BINARY_TARBALL_EXTRACTED_FOLDER/browser/chrome/icons/default
 install -Dvm644 "/usr/lib/${CARCH}-linux-gnu/libdbus-glib-1.so.2" "$_BINARY_TARBALL_EXTRACTED_FOLDER/usr/lib/libdbus-glib-1.so."2
 
 # Generate AppImage
-printf "\nGenerating AppImage\n";
-ARCH=${CARCH} $_APPIMAGETOOL_FILE --appimage-extract-and-run\
-  $_BINARY_TARBALL_EXTRACTED_FOLDER $_BUILD_APPIMAGE_FILE;
-chmod +x $_BUILD_APPIMAGE_FILE;
+printf "\nGenerating AppImage\n"
+ARCH=${CARCH} "$_APPIMAGETOOL_FILE" --appimage-extract-and-run \
+  "zsync|https://gitlab.com/api/v4/projects/24386000/packages/generic/librewolf/latest/LibreWolf.${CARCH}.AppImage.zsync" \
+  "$_BINARY_TARBALL_EXTRACTED_FOLDER" "$_BUILD_APPIMAGE_FILE"
+chmod +x "$_BUILD_APPIMAGE_FILE"
 
 # Move AppImage to specified location
-printf "\nMoving AppImage to specified location\n";
-mv $_BUILD_APPIMAGE_FILE $APPIMAGE_FILE;
+printf "\nMoving AppImage to specified location\n"
+mv "${_BUILD_APPIMAGE_FILE}" "${APPIMAGE_FILE}"
+mv "${_BUILD_APPIMAGE_FILE}.zsync" "${APPIMAGE_FILE}.zsync"
 
 # Cleanup files
-printf "\nCleaning up AppImage files\n";
-rm -rf $_BINARY_TARBALL_EXTRACTED_FOLDER;
-rm -f $_APPIMAGETOOL_FILE;
-rm -rf $_APPIMAGETOOL_EXTRACTED_FOLDER;
+printf "\nCleaning up AppImage files\n"
+rm -rf "$_BINARY_TARBALL_EXTRACTED_FOLDER"
+rm -f "$_APPIMAGETOOL_FILE"
+rm -rf "$_APPIMAGETOOL_EXTRACTED_FOLDER"
